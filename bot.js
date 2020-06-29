6d12b4b60cbe8cc55f5f59c35fece13a691e1234
alice15311 <alice15311@gmail.com>
2020-06-29 13:38:30 -0400

    added cmd serach tool and prefix

--- bot.js
+++ bot.js
@@ -1,29 +1,44 @@
-const Discord = require('discord.js');
-
+const fs = require('fs')
+const Discord = require("discord.js");
+const Client = require('./client/Client');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
- 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
-client.on('ready', () => {
 
-    console.log('I am ready!');
-
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
- 
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 
-// THIS  MUST  BE  THIS  WAY
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.content.indexOf(process.env.BOT_PREFIX) !== 0) return;
+    const args = message.content.slice(process.env.BOT_PREFIX.length).trim().split(/ +/g);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName);
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+    try {
+		command.execute(message);
+	} catch (error) {
+		console.error(error);
+		message.reply('There is no command called that!');
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
