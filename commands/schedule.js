844c109d5fb54e456793691fc727de78cf787c24
alice15311 <alice15311@gmail.com>
2020-06-30 17:59:12 -0400

    debug statements

--- commands/schedule.js
+++ commands/schedule.js
@@ -9,6 +9,7 @@ module.exports = {
         if(args != null){
             constant.hrtime = args[0];
             constant.mntime = args[1];
+            message.reply(args[0] + " " + args[1]);
             message.react(':white_check_mark:');
         }
     }
