5983fefe1afbe809a18b2b48dae16fe6484296d5
alice15311 <alice15311@gmail.com>
2020-06-30 19:57:24 -0400

    created say command

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+const { Util } = require("discord.js");
+
+module.exports = {
+    name: "say",
+    description: "say things and delete cmd message",
+
+    async execute(message){
+        const sayMessage = args.join(" ");
+        message.delete().catch(O_o=>{}); 
+        message.channel.send(sayMessage);
+    }
+}
--- commands/schedule.js
+++ commands/schedule.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "schedule movie time",
 
     async execute(message){
-        if(args[0] != null){
+        if(args[0] !== null){
             constant.hrtime = args[0];
             constant.mntime = args[1];
             message.channel.send(args[0] + ' ' + args[1]);
