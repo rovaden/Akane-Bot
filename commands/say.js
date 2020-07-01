cdc837ec1f9cd9b8d7d20d1e66928f4ef0318b5f
alice15311 <alice15311@gmail.com>
2020-06-30 20:33:00 -0400

    added await to fix cmd

--- commands/say.js
+++ commands/say.js
@@ -5,8 +5,9 @@ module.exports = {
     description: "say things and delete cmd message",
 
     async execute(message){
-        const sayMessage = args.join(" ");
+        const sayMessage =  await args.join(" ");
         message.delete().catch(O_o=>{}); 
         message.channel.send(sayMessage);
+        
     }
 }
