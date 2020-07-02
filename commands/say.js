41451ea6ba55cd9d4921e4838dc2029939a4b2bc
alice15311 <alice15311@gmail.com>
2020-07-02 18:52:32 -0400

    debug
    commented code out, logged stuff

--- commands/say.js
+++ commands/say.js
@@ -5,13 +5,14 @@ module.exports = {
     description: "say things and delete cmd message",
 
     async execute(message){
-        if(!args.length){
-            return message.reply("dont waste my time");
-        } else{
-        const sayMessage = args.join(" ");
-        message.delete().catch(O_o=>{}); 
-        message.channel.send(sayMessage);
-        }
+        console.log("cmd was received");
+        // if(!args.length){
+        //     return message.reply("dont waste my time");
+        // } else{
+        // const sayMessage = args.join(" ");
+        // message.delete().catch(O_o=>{}); 
+        // message.channel.send(sayMessage);
+        // }
         
     }
 }
