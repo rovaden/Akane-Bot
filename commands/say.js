b2ea12c3c04a4f4af7fccd99f740226643388f8f
alice15311 <alice15311@gmail.com>
2020-07-10 19:17:45 -0400

    say command should work after 10000 debug tries

--- commands/say.js
+++ commands/say.js
@@ -5,19 +5,13 @@ module.exports = {
     description: "say things and delete cmd message",
 
     async execute(message, args, commandName){
-        try{
-        console.log("cmd was received");
-        console.log(args.toString());}
-        catch (error) {
-            console.error(error);
+        if(!args.length){
+            return message.reply("dont waste my time");
+        } else{
+        const sayMessage = args.join(" ");
+        message.delete().catch(O_o=>{}); 
+        message.channel.send(sayMessage);
         }
-        // if(!args.length){
-        //     return message.reply("dont waste my time");
-        // } else{
-        // const sayMessage = args.join(" ");
-        // message.delete().catch(O_o=>{}); 
-        // message.channel.send(sayMessage);
-        // }
         
     }
 }
