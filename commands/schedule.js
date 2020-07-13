5afb508a32f8050145b4e81f0c128ae39f57ca85
alice15311 <alice15311@gmail.com>
2020-07-12 23:55:00 -0400

    did in correct scope

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,16 +1,17 @@
 const { Util } = require("discord.js");
 const fs = require('fs');
+const values = require('../data storage/values.json');
 
 module.exports = {
     name: "schedule",
     description: "schedule movie time",
 
     async execute(message, args){
-        const m = JSON.parse(fs.readFileSync('../data storage/values.json').toString());
+        const m = JSON.parse(fs.readFileSync(values).toString());
         var movieargs = [args[0], args[1], args[2]];
         console.log(movieargs.toString());
         m.movietime = movieargs;
         console.log(m.movietime)
-        fs.writeFile('../data storage/values.json', JSON.stringify(m));
+        fs.writeFile(values, JSON.stringify(m));
     }
 }
