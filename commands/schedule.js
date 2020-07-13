408a705f99f8049dd74f9a3a6623953d26607a6a
alice15311 <alice15311@gmail.com>
2020-07-12 23:36:18 -0400

    redid schedule cmd

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,18 +1,15 @@
 const { Util } = require("discord.js");
-const constant = require("../variables.js")
+const fs = require('fs');
 
 module.exports = {
     name: "schedule",
     description: "schedule movie time",
 
-    async execute(message){
-        if(args[0] !== null){
-            constant.hrtime = args[0];
-            constant.mntime = args[1];
-            message.channel.send(args[0] + ' ' + args[1]);
-            message.react(':white_check_mark:');
-        } else{
-            message.channel.send('u fuckwad, put in an arguement');
-        }
+    async execute(message, args){
+        var m = JSON.parse(fs.readFileSync('values.json').toString());
+        var movieargs = [args[0], args[1], args[2]];
+        console.log(movieargs.toString());
+        m.movietime = movieargs;
+        fs.writeFile('values.json', JSON.stringify(m));
     }
 }
--- variables.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "variables",
-    desc: "holds all variables for ongoing commands",
-    constructor(){
-    let hrtime = 0;
-    let mntime = 0;
-    }
-}
