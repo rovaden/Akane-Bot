f72e96064dacc2dd17587eb37f9f73191bd0fac8
alice15311 <alice15311@gmail.com>
2020-07-13 00:38:55 -0400

    redid with no auto parse

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,13 +1,12 @@
 const { Util } = require("discord.js");
 const fs = require('fs');
-const values = require('../data storage/values.json');
+const m = JSON.parse(fs.readFileSync(values).toString());
 
 module.exports = {
     name: "schedule",
     description: "schedule movie time",
 
     async execute(message, args){
-        const m = JSON.parse(fs.readFileSync(values).toString());
         var movieargs = [args[0], args[1], args[2]];
         console.log(movieargs.toString());
         m.movietime = movieargs;
