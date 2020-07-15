62c443e2522279feb9cdffe243b7c586712cf7b5
alice15311 <alice15311@gmail.com>
2020-07-15 19:06:23 -0400

    justin worked his magic

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,16 +1,15 @@
 const { Util } = require("discord.js");
 const fs = require('fs');
-const m = JSON.parse(fs.readFileSync(values).toString());
 
 module.exports = {
     name: "schedule",
     description: "schedule movie time",
 
     async execute(message, args){
+        let m = JSON.parse(fs.readFileSync("./commands/data/values.json").toString());
         var movieargs = [args[0], args[1], args[2]];
-        console.log(movieargs.toString());
         m.movietime = movieargs;
-        console.log(m.movietime)
-        fs.writeFile(values, JSON.stringify(m));
+        console.log(m);
+        fs.writeFileSync("./commands/data/values.json" , JSON.stringify(m));
     }
 }
