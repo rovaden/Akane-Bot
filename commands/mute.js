a0c3622b2ef315d716fab037cf0a59960f65b925
alice15311 <alice15311@gmail.com>
2020-12-10 18:22:31 -0500

    slight change in perm fileter

--- commands/mute.js
+++ commands/mute.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "mutes people, not bots",
 
     async execute(message, args, commandName){
-        if (message.author.roles.includes("768214883808313384" || "743330976892977277") != true ){
+        if (message.author.roles.includes("768214883808313384") != true ){
             message.reply("no fuck you. u dont got the perms");
             return null;
         };
