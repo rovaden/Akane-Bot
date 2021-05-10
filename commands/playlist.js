3fb99ef9ed0904dcf8aac5e9d33ca865a099f519
alice15311 <alice15311@gmail.com>
2021-05-10 18:39:31 -0400

    changed embed message

--- commands/playlist.js
+++ commands/playlist.js
@@ -8,10 +8,11 @@ module.exports = {
         const playlistEmbed = new Discord.MessageEmbed()
 	    .setColor('#0099ff')
 	    .setTitle('Newt Spotify Playlist')
+        .setDescription("add songs you really like. this is meant to be representative of the server's music tastes.")
 	    .setURL('https://open.spotify.com/playlist/2S2EOkAqyQKxUop6LBRkd1?si=eaad1a3273674e0e')
 	    .setImage('https://i.imgur.com/R6OHMKc.png')
 	    .setTimestamp()
-	    .setFooter('add whatever you want, no one cares.');
+	    .setFooter('help im in constant pain');
 
         message.channel.send(playlistEmbed);
     }
