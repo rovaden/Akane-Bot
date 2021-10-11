bb80221e10f81a4f95c64441bf73bd965da8457d
alice15311 <57412796+alice15311@users.noreply.github.com>
2021-10-11 19:35:43 -0400

    added checking what the prefix is set to

--- commands/prefix.js
+++ commands/prefix.js
@@ -6,21 +6,25 @@ module.exports = {
 
     async execute(message, args, commandName, db, mgclient){
         // process.env.BOT_PREFIX = args
-        let user = message.member;
-        const col = db.collection("settings");
-        if(!user || user.bot == true || !user.hasPermission(`ADMINISTRATOR`) ){
-            message.channel.send(`I can't unmute ${user} because you made an oopsie.`);
-            return null;
+        if (args[0] == null) {
+            message.reply("the prefix is currently: " + process.env.BOT_PREFIX);
+        } else {
+            let user = message.member;
+            const col = db.collection("settings");
+            if(!user || user.bot == true || !user.hasPermission(`ADMINISTRATOR`) ){
+                message.channel.send(`I can't unmute ${user} because you made an oopsie.`);
+                return null;
+            }
+            col.updateOne( {BOT_PREFIX : {$exists : true}},
+            {
+              $set: {
+                BOT_PREFIX: args[0]
+              }
+            });
+            const settings = await db.collection("settings").findOne({BOT_PREFIX : { $exists : true}});
+            console.log("prefix: " + settings.BOT_PREFIX)
+            process.env.BOT_PREFIX = settings.BOT_PREFIX.toString();
+            message.reply(args + " has been set to the bot prefix!")
         }
-        col.updateOne( {BOT_PREFIX : {$exists : true}},
-        {
-          $set: {
-            BOT_PREFIX: args[0]
-          }
-        });
-        const settings = await db.collection("settings").findOne({BOT_PREFIX : { $exists : true}});
-        console.log("prefix: " + settings.BOT_PREFIX)
-        process.env.BOT_PREFIX = settings.BOT_PREFIX.toString();
-        message.reply(args + " has been set to the bot prefix!")
     }
 }
